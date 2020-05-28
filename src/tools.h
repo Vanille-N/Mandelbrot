dc261492c579d3f0ed66fbff7d8709e8ffe57b83
Vanille-N <neven.villani@gmail.com>
2020-05-28 08:24:34 +0200

    comments for tools.h

--- src/tools.h
+++ src/tools.h
@@ -1,11 +1,27 @@
 #pragma once
 
+/*
+ * This file groups all major enums and structs used throughout the project
+ */
+
+/*
+ * Methods implemented in ioterm
+ */
 struct rgb {
     int r ;
     int g ;
     int b ;
+
+    void display () ;
+    void preview () ;
 };
 
+/*
+ * Token identifiers
+ * Enum starts at -1000 so that positive values can be reserved for
+ * actual numerals. This means that a token in actually either a cmd, or
+ * a positive integer casted to a cmd.
+ */
 enum cmd {
     SCOPE=-1000, NIL, REC, MAP, MAKE, SAVE, LS, HELP,
     RESET, HASH, NEXT, LSIDE, RSIDE, USIDE, DSIDE, HSIDE, VSIDE,
@@ -20,20 +36,36 @@ enum msg_log {
     SIGLS, SIGRESET, SIGHELP, NEWFOCUS, EXCEPTION, FLIP, NPORTABLE,
 };
 
+/*
+ * Categories of character distinguished by the tokenizer.
+ * A word boundary is whenever at least one of the two adjacent characters
+ * is neither a KEYWORD nor a NUMERIC, nor accessed without spaces from a
+ * MODIFIER. All non-alphabetic keywords are a single character
+ * long.
+ */
 enum types_chr {
     KEYWORD, SELECTOR, INDICATOR, MODIFIER,
     SYMBOL, UNKNOWN, BLANK, NUMERIC
 };
 
+/*
+ * To remember user choices between executions
+ */
 enum option {
     OPT_INIT, OPT_DENY, OPT_ALLOW,
 };
 
+/*
+ * A slice of the command. Created by the tokenizer and read by the lexer.
+ */
 struct slice {
     int beg ;
     int len ;
 };
 
+/*
+ * Zoom command
+ */
 struct adjust {
     bool active ;
     int indicator ;
